bc2aa5001964b845bb8faab1c334fbea0061fe60
Madhur Jain <42210565+madhurDroiddev@users.noreply.github.com>
2023-12-14 12:25:03 +0530

    Update base_bloc.dart

--- base_bloc.dart
+++ base_bloc.dart
@@ -29,3 +29,50 @@ abstract class BaseBloc<VS, E> {
     eventController.sink.add(event);
   }
 }
+
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_pag_demo/core/base_bloc.dart';
+
+abstract class BaseBlocState<T extends StatefulWidget, B extends BaseBloc, VS,
+    E> extends State<T> {
+  late B bloc;
+  late VS viewState;
+  late StreamSubscription _eventsSubscription;
+
+  VS get initialViewState;
+
+  B get initializeBloc;
+
+  @override
+  void initState() {
+    super.initState();
+
+    viewState = initialViewState;
+    bloc = initializeBloc;
+    _subscribe();
+  }
+
+  void _subscribe() {
+    _eventsSubscription = (bloc.eventController.stream as Stream<E>)
+        .distinct()
+        .listen(listenEvents);
+  }
+
+  void listenEvents(E event);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<VS>(
+        stream: (bloc.viewStateStream as Stream<VS>).distinct(),
+        builder: (buildContext, snapshot) {
+          viewState = snapshot.data!;
+          return buildViewState(viewState, context);
+        });
+  }
+
+  Widget buildViewState(VS viewState, BuildContext context);
+}
+
