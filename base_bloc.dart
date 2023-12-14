859a906d60940b9cc1553a8b899bb3e72b664261
Madhur Jain <42210565+madhurDroiddev@users.noreply.github.com>
2023-12-14 12:24:26 +0530

    Update base_bloc.dart

--- base_bloc.dart
+++ base_bloc.dart
@@ -1,23 +1,31 @@
-abstract class BaseBloc<VS>{
-  late VS viewState;
+import 'dart:async';
+
+import 'base_repo.dart';
+
+abstract class BaseBloc<VS, E> {
+  VS viewState;
   late Stream<VS> viewStateStream;
   late StreamSink<VS> viewStateSink;
   late StreamSubscription<VS> viewStateSubscription;
   late StreamController<VS> viewStateController;
+  late StreamController<E> eventController;
 
   dispose();
 
-  VS get initialViewState;
-
-  BaseBloc(){
-    viewState = initialViewState;
+  BaseBloc(this.viewState) {
     viewStateController = StreamController.broadcast();
     viewStateStream = viewStateController.stream;
     viewStateSink = viewStateController.sink;
+    dispatchViewState(viewState);
     viewStateSubscription = viewStateStream.listen((event) {});
   }
-  
+
   dispatchViewState(VS viewState) {
+    this.viewState = viewState;
     viewStateSink.add(viewState);
+  }
+
+  dispatchEvent(E event) {
+    eventController.sink.add(event);
   }
 }
